f3df7430b5d9e60703dcf502a8f057c21b698c5f
simonjcarr <simonjcarr@gmail.com>
2022-12-30 09:54:49 +0000

    bug fix

--- start/routes.ts
+++ start/routes.ts
@@ -22,7 +22,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.resource('node-types', 'NodeTypeController')
-  Route.resource('nodes', 'NodeController')
+  Route.resource('nodes', 'NodesController')
   Route.resource('tags', 'TagsController')
 }).prefix('api/v1')
 
